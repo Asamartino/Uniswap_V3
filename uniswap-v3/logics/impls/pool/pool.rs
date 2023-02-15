a1ab44374e0f3c8528c72e382702adab31b0746a
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-02-15 13:53:30 +0900

    cargo fmt

--- uniswap-v3/logics/impls/pool/pool.rs
+++ uniswap-v3/logics/impls/pool/pool.rs
@@ -1,47 +1,89 @@
 use crate::traits::factory::FactoryRef;
 
-pub use crate::{
-    impls::pool::*,
-    traits::pool::*,
-};
+pub use crate::{impls::pool::*, traits::pool::*};
 use openbrush::{
-    contracts::{
-        ownable::*,
-        psp22::*,
-        reentrancy_guard::*,
-        traits::psp22::PSP22Ref,
-    },
-    traits::{
-        AccountId,
-        Balance,
-        Storage,
-        Timestamp,
-    }
+    contracts::{ownable::*, psp22::*, reentrancy_guard::*, traits::psp22::PSP22Ref},
+    traits::{AccountId, Balance, Storage, Timestamp},
 };
 pub trait Internal {
-    fn _emit_initialize_event(&self ,sqrt_price_x96: u128, tick: i32) -> Result<(), PoolError>;
-    fn _emit_mint_event(&self, sender:AccountId, recipient: AccountId, tick_lower: i32, tick_upper: i32, amount: Balance, amount0: Balance, amount1: Balance) -> Result<(), PoolError>;
-    fn _emit_collect_event(&self, sender:AccountId, recipient: AccountId, tick_lower: i32, tick_upper: i32, amount0: Balance, amount1: Balance) -> Result<(), PoolError>;
-    fn _emit_burn_event(&self, sender:AccountId, tick_lower: i32, tick_upper: i32, amount: Balance, amount0: Balance, amount1: Balance) -> Result<(), PoolError>;
-    fn _emit_swap_event(&self, sender:AccountId, recipient: AccountId, amount0: Balance, amount1: Balance, sqrt_price_x96: u128, liquidity: u128, tick: i32) -> Result<(), PoolError>;
-    fn _emit_flash_event(&self, sender:AccountId, recipient: AccountId,amount0: Balance, amount1: Balance, paid0: u128, paid1: u128) -> Result<(), PoolError>;
-    fn _emit_set_fee_protocol_event(&self, fee_protocol0_old: u8,fee_protocol1_old: u8, fee_protocol0_new: u8, fee_protocol1_new: u8,) -> Result<(), PoolError>;
-    fn _emit_collect_protocol_event(&self, sender:AccountId, recipient: AccountId, amount0: Balance, amount1: Balance) -> Result<(), PoolError>;
+    fn _emit_initialize_event(&self, sqrt_price_x96: u128, tick: i32) -> Result<(), PoolError>;
+    fn _emit_mint_event(
+        &self,
+        sender: AccountId,
+        recipient: AccountId,
+        tick_lower: i32,
+        tick_upper: i32,
+        amount: Balance,
+        amount0: Balance,
+        amount1: Balance,
+    ) -> Result<(), PoolError>;
+    fn _emit_collect_event(
+        &self,
+        sender: AccountId,
+        recipient: AccountId,
+        tick_lower: i32,
+        tick_upper: i32,
+        amount0: Balance,
+        amount1: Balance,
+    ) -> Result<(), PoolError>;
+    fn _emit_burn_event(
+        &self,
+        sender: AccountId,
+        tick_lower: i32,
+        tick_upper: i32,
+        amount: Balance,
+        amount0: Balance,
+        amount1: Balance,
+    ) -> Result<(), PoolError>;
+    fn _emit_swap_event(
+        &self,
+        sender: AccountId,
+        recipient: AccountId,
+        amount0: Balance,
+        amount1: Balance,
+        sqrt_price_x96: u128,
+        liquidity: u128,
+        tick: i32,
+    ) -> Result<(), PoolError>;
+    fn _emit_flash_event(
+        &self,
+        sender: AccountId,
+        recipient: AccountId,
+        amount0: Balance,
+        amount1: Balance,
+        paid0: u128,
+        paid1: u128,
+    ) -> Result<(), PoolError>;
+    fn _emit_set_fee_protocol_event(
+        &self,
+        fee_protocol0_old: u8,
+        fee_protocol1_old: u8,
+        fee_protocol0_new: u8,
+        fee_protocol1_new: u8,
+    ) -> Result<(), PoolError>;
+    fn _emit_collect_protocol_event(
+        &self,
+        sender: AccountId,
+        recipient: AccountId,
+        amount0: Balance,
+        amount1: Balance,
+    ) -> Result<(), PoolError>;
 }
-impl<T: Storage<data::Data> // + Storage<psp22::Data> + Storage<ownable::Data>
-> Pool for T {
+impl<
+        T: Storage<data::Data>, // + Storage<psp22::Data> + Storage<ownable::Data>
+    > Pool for T
+{
     fn initialize(
         &mut self,
         token_0: AccountId,
         token_1: AccountId,
-        fee: u32
+        fee: u32,
     ) -> Result<(), PoolError> {
         self.data::<data::Data>().token_0 = token_0;
         self.data::<data::Data>().token_1 = token_1;
         self.data::<data::Data>().fee = fee;
         Ok(())
     }
-
 
     fn get_token_0(&self) -> AccountId {
         self.data::<data::Data>().token_0
@@ -54,36 +96,36 @@ impl<T: Storage<data::Data> // + Storage<psp22::Data> + Storage<ownable::Data>
     fn get_fee(&self) -> u32 {
         self.data::<data::Data>().fee
     }
-    
-    fn get_tick_spacing(&self) -> i32{
+
+    fn get_tick_spacing(&self) -> i32 {
         self.data::<data::Data>().tick_spacing
     }
-    
-    fn get_max_liquidity_per_tick(&self) -> u128{
+
+    fn get_max_liquidity_per_tick(&self) -> u128 {
         self.data::<data::Data>().max_liquidity_per_tick
     }
-    
+
     // fn get_slot0(&self) -> Slot{
     //     self.data::<data::Data>().slot0
     // }
-    
-    fn get_fee_growth_global_0x128(&self) -> u128{
+
+    fn get_fee_growth_global_0x128(&self) -> u128 {
         self.data::<data::Data>().fee_growth_global_0x128
     }
-    
-    fn get_fee_growth_global_1x128(&self) -> u128{
+
+    fn get_fee_growth_global_1x128(&self) -> u128 {
         self.data::<data::Data>().fee_growth_global_1x128
     }
-    
+
     // fn get_protocol_fees(&self) -> ProtocolFees{
     //     self.data::<data::Data>().protocol_fees
     // }
-    
-    fn get_liquidity(&self) -> u128{
+
+    fn get_liquidity(&self) -> u128 {
         self.data::<data::Data>().liquidity
     }
-    
-    fn get_tick_bitmap(&self, entry: i32) -> Option<u128>{
+
+    fn get_tick_bitmap(&self, entry: i32) -> Option<u128> {
         self.data::<data::Data>().tick_bitmap.get(&entry)
     }
 
@@ -122,13 +164,12 @@ impl<T: Storage<data::Data> // + Storage<psp22::Data> + Storage<ownable::Data>
     //     let flipped_lower = false;
     //     let flipped_upper = false;
 
-        
     // }
 
     // fn get_position(&self, owner: AccountId, tick_lower: i32, tick_upper: i32)  -> Option<PositionInfo> {
     //     self.data::<data::Data>().positions[Self::env().hash_encoded::<Blake2x256,_>(&(owner, tick_lower, tick_upper))]
     // }
-    
+
     // fn _modify_position(&mut self, owner: AccountId, tick_lower: i32, tick_upper: i32, liqudiity_delta: i128) -> Result<Balance, PoolError>{
     //     if !self._check_ticks(tick_lower,tick_upper){
     //         return Err(PoolError::TickError)
@@ -136,8 +177,7 @@ impl<T: Storage<data::Data> // + Storage<psp22::Data> + Storage<ownable::Data>
 
     // }
 
-    fn _check_ticks(&self, tick_lower: i32, tick_upper: i32) -> bool{
+    fn _check_ticks(&self, tick_lower: i32, tick_upper: i32) -> bool {
         tick_lower < tick_upper && tick_lower >= i32::MIN && tick_upper <= i32::MAX
     }
-
 }
